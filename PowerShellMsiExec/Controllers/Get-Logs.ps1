2b6caf1ea01e3b6fc9f360aa9824c4b0e07c57ac
Ava Hazel <ava@ava>
2022-10-14 20:26:52 -0500

    Added MsiExec Examples

---
+++ PowerShellMsiExec/Controllers/Get-Logs.ps1
@@ -0,0 +1,18 @@
+Function Controller
+{
+    $Path = Split-Path -Path $MyInvocation.PSScriptRoot
+    Import-Module "$Path"
+
+    Get-Logs -Path C:\Logs
+}
+
+$ResultArray = Controller
+
+Foreach($Result in $ResultArray)
+{
+    Write-Host ""
+    Write-Host $Result.OperationType
+    Write-Host $Result.ProductInfo
+    Write-Host $Result.ErrorStatus
+    Write-Host ""
+}
